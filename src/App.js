0b5d64f99ccb54da1c3c1a0a4613387ff74ec92a
whl961210 <whl961210@gmail.com>
2023-08-16 23:17:14 +0800

    fixed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import Tuiter from "./foodadvisor";
-import {HashRouter, Navigate} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
-
+import {Navigate} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-      <HashRouter>
-
-        <div className="container">
-
-          <Routes>
-            <Route path="/"         element={<Navigate to="/tuiter/home"/>}/>
-            <Route path="/tuiter/*" element={<Tuiter/>}/>
-
-          </Routes>
-
-        </div>
-      </HashRouter>
-
+    <HashRouter>
+  <div className="container">
+      <Routes>
+      <Route path="/"         element={<Navigate to="/tuiter"/>}/>
+      <Route path="/tuiter/*" element={<Tuiter/>}/>
+    </Routes>
+  </div>
+</HashRouter>
   );
 }
 export default App;
